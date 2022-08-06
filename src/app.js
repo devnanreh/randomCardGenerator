34903fa08a8510c5f70ef3d8f6069e9d1c038d49
nanher94 <hernan.albiol@gmail.com>
2022-08-06 11:02:21 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = [
+    '<i class="bi bi-suit-heart-fill"></i>',
+    '<i class="bi bi-suit-club-fill"></i>',
+    '<i class="bi bi-suit-diamond-fill"></i>',
+    '<i class="bi bi-suit-spade-fill"></i>'
+  ];
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  const randomGenerate = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  function generateCard() {
+    let randomPalo = randomGenerate(palos);
+    let randomNum = randomGenerate(numeros);
+    // Reemplazar Numero
+    let changeNumber = document.querySelector(".number");
+    changeNumber.innerHTML = randomNum;
+    // Reemplazar Palo y asignar Color
+    let changePalo = document.querySelectorAll(".palo");
+    changePalo.forEach(item => {
+      item.innerHTML = randomPalo;
+      if (
+        randomPalo === '<i class="bi bi-suit-heart-fill"></i>' ||
+        randomPalo === '<i class="bi bi-suit-diamond-fill"></i>'
+      ) {
+        (item.style.color = "red") && (changeNumber.style.color = "red");
+      } else {
+        (item.style.color = "black") && (changeNumber.style.color = "black");
+      }
+    });
+  }
+  document.querySelector("#randomCardButton").onclick = function() {
+    generateCard();
+  };
+  // Intervalo de 10s
+  setInterval(() => {
+    generateCard();
+  }, 10000);
 };
